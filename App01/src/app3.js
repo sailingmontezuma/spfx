afebd5094885b95f7f73cae0c9f35b612030ceb5
Jakub Nowakowski <jakub.nowakowski.home@outlook.com>
2017-12-09 10:56:30 +0100

    app3

---
+++ App01/src/app3.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchableTable from './SearchableTable';
+import {data} from './data';
+ 
+// Filterable CheatSheet Component
+ReactDOM.render( 
+    <SearchableTable data={data}/>, 
+    document.getElementById("reactor"));
