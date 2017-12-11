8af3ab9eef3d5f625cd0f3881b83b6a3f64c0e71
Jakub Nowakowski <jakub.nowakowski.home@outlook.com>
2017-12-11 07:54:13 +0100

    handle errors

--- NodejsApp/app.js
+++ NodejsApp/app.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
 
+
+app.use(morgan("dev"))
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
-// app.use((req, res, next) => {
-//     res.status(200).json({
-//         message: "it works"
-//     });  
-// });
+app.use((req, res, next) => {
+    const error = new Error("paage not found");
+    error.status = 404;
+    next(error);
+});
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        message: error.message,
+        description: "custom error description"
+    });
+});
 
 module.exports = app;
