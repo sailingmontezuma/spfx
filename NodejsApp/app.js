e852e6dd4b771552da6182509417173a08fe54f5
Jakub Nowakowski <jakub.nowakowski.home@outlook.com>
2017-12-11 08:23:49 +0100

    options

--- NodejsApp/app.js
+++ NodejsApp/app.js
@@ -12,9 +12,20 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+
 app.use((req, res, next) => {
     const error = new Error("paage not found");
     error.status = 404;
